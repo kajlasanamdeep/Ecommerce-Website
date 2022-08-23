4d38e0db8aab1d9acda8d1b35cdb0f52fb025c38
shubham saini <shubhamsaini759@gmail.com>
2022-08-23 17:17:34 +0530

    frontend-setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Footer from './comopnents/footer'
+import Navbar from './comopnents/navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </>
+  )
 }
 
 export default MyApp
